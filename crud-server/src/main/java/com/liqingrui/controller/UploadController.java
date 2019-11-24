e393664196c2359fe7bd41532051f8c20afbf5bb
liqingrui <2636045725@qq.com>
2019-11-24 18:04:28 +0800

    配置中心测试

---
+++ crud-server/src/main/java/com/liqingrui/controller/UploadController.java
@@ -0,0 +1,40 @@
+package com.liqingrui.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RequestMapping("/file")
+@CrossOrigin
+@RestController
+public class UploadController {
+    @Value("${file.domain}")
+    private String fileDomain;
+    @Value("${file.path}")
+    private String filePath;
+
+    @PostMapping("/upload")
+    public void upload(@RequestParam(name = "file")MultipartFile file){
+        Map<String,Object> map = new HashMap<>();
+        map.put("result",true);
+//        文件名
+        String fileName = UUID.randomUUID()+".jpg";
+//        文件保存地址
+        File destPath = new File(filePath + fileName);
+//        文件上传
+        try {
+            file.transferTo(destPath);
+            map.put("result",true);
+            map.put("fileName",fileName);
+            map.put("imgUrl",fileDomain+fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
